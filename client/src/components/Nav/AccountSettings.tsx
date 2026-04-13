4129e7a01d4e488b3a7669dc824a1064f2a96ea0
sudipto68 <sudiptomitro2016@gmail.com>
2026-04-13 12:49:09 +0600

    change first name to username on all places

--- client/src/components/Nav/AccountSettings.tsx
+++ client/src/components/Nav/AccountSettings.tsx
@@ -57,7 +57,7 @@ function AccountSettings({ collapsed = false }: { collapsed?: boolean }) {
         }}
       >
         <div className="text-token-text-secondary ml-3 mr-2 py-2 text-sm" role="note">
-          {user?.email ?? localize('com_nav_user')}
+          {user?.username ?? localize('com_nav_user')}
         </div>
         <DropdownMenuSeparator />
         {startupConfig?.balance?.enabled === true && balanceQuery.data != null && (
