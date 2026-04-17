36d6b18253700c153d0993e6227abb7dcbf1ab6b
amit <amit210905@gmail.com>
2026-04-17 19:40:37 +0600

    Ref: child-friendly login page design for AYO

--- client/src/components/Auth/SocialButton.tsx
+++ client/src/components/Auth/SocialButton.tsx
@@ -6,10 +6,10 @@ const SocialButton = ({ id, enabled, serverDomain, oauthPath, Icon, label }) => 
   }
 
   return (
-    <div className="mt-2 flex gap-x-2">
+    <div className="mt-2 flex justify-center gap-x-2">
       <a
         aria-label={`${label}`}
-        className="flex w-full items-center space-x-3 rounded-2xl border border-border-light bg-surface-primary px-5 py-3 text-text-primary transition-colors duration-200 hover:bg-surface-tertiary"
+        className="flex max-w-[300px] items-center justify-center space-x-3 rounded-2xl border border-border-light bg-surface-primary p-3 text-text-primary transition-colors duration-200 hover:bg-surface-tertiary"
         href={`${serverDomain}/oauth/${oauthPath}`}
         data-testid={id}
       >
