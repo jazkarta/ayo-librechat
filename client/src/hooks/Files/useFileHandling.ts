a534cef14c9380c46070a7bdf5fe087eb6724e16
amit <amit210905@gmail.com>
2026-04-30 13:11:33 +0600

    Ref: change error message for file is too large

--- client/src/hooks/Files/useFileHandling.ts
+++ client/src/hooks/Files/useFileHandling.ts
@@ -174,6 +174,8 @@ const useFileHandlingCore = (params: UseFileHandling | undefined, fileState: Fil
 
         if (error?.code === 'ERR_CANCELED') {
           errorMessage = 'com_error_files_upload_canceled';
+        } else if (error?.response?.status === 413) {
+          errorMessage = localize('com_error_files_upload_too_large', { 0: 20 });
         } else if (error?.response?.data?.message) {
           errorMessage = error.response.data.message;
         }
